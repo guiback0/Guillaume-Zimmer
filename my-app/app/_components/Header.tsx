6c082e9a03db5013789350682cd880e73704b8d6
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-07-16 00:08:45 +0200

    new metadata and github header link

--- my-app/app/_components/Header.tsx
+++ my-app/app/_components/Header.tsx
@@ -1,12 +1,25 @@
+import { Button } from "@/components/ui/button";
 import { Section } from "./Section";
+import { GithubIcon } from "./icons/Githubicon";
+import { Link } from "react-router-dom";
+import { cn } from "@/lib/utils";
 
 export const Header = () => {
    return (
       <header className="sticky top-0 py-4">
          <Section className="flex items-baseline">
             <h1 className="text-lg font-bold text-primary">Guillaume Zimmer</h1>
-
-            <div className="flex1"></div>
+            <div className="flex-1" />
+            <ul>
+               <Link
+                  href="https://github.com/guiback0"
+                  className={cn(
+                     buttonVariants({ variant: "outline" }),
+                     "size-6 p-0"
+                  )}>
+                  <GithubIcon size={16} className="text-foreground" />
+               </Link>
+            </ul>
          </Section>
       </header>
    );
