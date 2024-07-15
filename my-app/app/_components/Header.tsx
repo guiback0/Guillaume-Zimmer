44b7f62eaf80da9e641e109a3ca8974b780a24a4
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-07-16 00:29:39 +0200

    linkedin header link

--- my-app/app/_components/Header.tsx
+++ my-app/app/_components/Header.tsx
@@ -1,8 +1,9 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Section } from "./Section";
-import { GithubIcon } from "./icons/Githubicon";
+import { GithubIcon } from "./icons/GithubIcon";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
 
 export const Header = () => {
    return (
@@ -10,7 +11,7 @@ export const Header = () => {
          <Section className="flex items-baseline">
             <h1 className="text-lg font-bold text-primary">Guillaume Zimmer</h1>
             <div className="flex-1" />
-            <ul>
+            <ul className="flex items-center gap-2">
                <Link
                   href="https://github.com/guiback0"
                   className={cn(
@@ -18,6 +19,14 @@ export const Header = () => {
                      "size-6 p-0"
                   )}>
                   <GithubIcon size={16} className="text-foreground" />
+               </Link>
+               <Link
+                  href="https://www.linkedin.com/in/guillaume-zimmer/"
+                  className={cn(
+                     buttonVariants({ variant: "outline" }),
+                     "size-6 p-0"
+                  )}>
+                  <LinkedinIcon size={16} className="text-foreground" />
                </Link>
             </ul>
          </Section>
