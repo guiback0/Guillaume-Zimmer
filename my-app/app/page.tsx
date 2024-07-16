aca4ae8b41b9b278452cc53c2622bd29691b205b
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-07-17 00:14:46 +0200

    spacing component

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./_components/Header";
 import { Hero } from "./_components/Hero";
+import { Spacing } from "./_components/Spacing";
 
 export default function Home() {
    return (
       <main>
          <Header />
+         <Spacing size="md" />
          <Hero />
       </main>
    );
